ef748eb809e93f974d98be6d94a256a9a4a09525
bendyfan11 <108228795+bendyfan11@users.noreply.github.com>
2026-04-08 13:50:00 -0500

    Update message in Hi.py for clarity

--- PythonProjects/Hi.py
+++ PythonProjects/Hi.py
@@ -1,1 +1,1 @@
-print("Hi. This file exists because, for some reason, Github wont let me make an empty folder in Main.")
+print("Hi. This file exists because, for some reason, Github wont let me make an empty folder in Main. Expect Duplicate projects, my sorting is atrocious.")
