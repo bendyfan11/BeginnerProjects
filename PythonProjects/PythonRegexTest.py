7122116e7d10a4b7c349d0d20db903197413f7d5
bendyfan11 <108228795+bendyfan11@users.noreply.github.com>
2026-04-08 16:17:00 -0500

    Fix email validation regex and input handling

--- PythonProjects/PythonRegexTest.py
+++ PythonProjects/PythonRegexTest.py
@@ -1,8 +1,8 @@
 #Email Validator Doohicky
 import re
-Email = input("What is your email?")
-Valid = r"^([A-Za-z0-9._%+-])+@([A-Za-z0-9.-])+\.+([a-zA-Z]{2,})$"
-if re.fullmatch(valid, email):
+Email = input("What is your email? ")
+Valid = r"^[A-Za-z0-9._%+-]+@([A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.)+[a-zA-Z]{2,}$"
+if re.fullmatch(Valid, Email):
   print("This is a valid Email.")
 else:
-  print(f"Error. Expected format: X@X.x. Got {Email}.)
+  print(f"Error. Expected format: X@X.x. Got {Email}.")
